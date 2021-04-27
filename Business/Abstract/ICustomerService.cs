c08edb3bd0cce86e3917b94f4c9ac225b8684e4b
Raqif Teymurov <raqif.teymurov.585@teams.dia.edu.az>
2021-04-28 00:46:51 +0400

    business

---
+++ Business/Abstract/ICustomerService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+  public  interface ICustomerService
+    {
+        void Add(Customer customer);
+        void Update(Customer customer);
+        Customer GetbyId(int id);
+        List<Customer> GetAll();
+
+    }
+}
