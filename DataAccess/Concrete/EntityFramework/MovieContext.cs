15f1a5711cc20b19d3b27f31e3e66cc2bccfa792
Raqif Teymurov <raqif.teymurov.585@teams.dia.edu.az>
2021-04-28 00:09:22 +0400

    Entities+DataAccess

---
+++ DataAccess/Concrete/EntityFramework/MovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+   public class MovieContext:DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Customer:IEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string ChosenMovie { get; set; }
+
+    }
+}
