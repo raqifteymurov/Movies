f7b1bdb76d1477ad6b5cf64096150a375883a1eb
Raqif Teymurov <raqif.teymurov.585@teams.dia.edu.az>
2021-04-28 18:24:24 +0400

    Added new table

---
+++ Business/Abstract/IAboutMovieService.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IAboutMovieService
+    {
+        void Add(AboutMovie movie);
+        void Update(AboutMovie movie);
+        void Delete(AboutMovie movie);
+        AboutMovie GetById(int id);
+        List<AboutMovie> GetAll();
+
+    }
+}
---
+++ Business/Concrete/AboutMovieManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class AboutMovieManager : IAboutMovieService
+    {
+        private IAboutMovieDal _aboutmovieDal;
+
+        public AboutMovieManager(IAboutMovieDal aboutmovieDal)
+        {
+            _aboutmovieDal = aboutmovieDal;
+        }
+
+        public void Add(AboutMovie movie)
+        {
+            _aboutmovieDal.Add(movie);
+        }
+
+        public void Delete(AboutMovie movie)
+        {
+            _aboutmovieDal.Delete( movie);
+        }
+
+        public List<AboutMovie> GetAll()
+        {
+            return _aboutmovieDal.GetAll();
+        }
+
+        public AboutMovie GetById(int id)
+        {
+            return _aboutmovieDal.Get(s => s.Id == id);
+        }
+
+        public void Update(AboutMovie movie)
+        {
+            _aboutmovieDal.Update(movie);
+        }
+    }
+}
